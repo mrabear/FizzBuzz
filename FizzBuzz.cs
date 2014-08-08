c0b8510714ab6078c8653c2d81c3467487869ec0
Brian Hebert <mrabear@gmail.com>
2014-08-08 15:13:43 -0400

    Solution Commit

---
+++ FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+class FizzBuzz
+{
+    static void Main()
+    {
+        Debug.WriteLine("\n############## Starting FizzBuzz ##############");
+
+        for (int lc = 1; lc <= 100; lc++)
+        {
+            if (lc % 3 == 0 && lc % 5 == 0)
+                Debug.WriteLine("FizzBuzz");
+            else if (lc % 3 == 0 )
+                Debug.WriteLine("Fizz");
+            else if (lc % 5 == 0)
+                Debug.WriteLine("Buzz");
+            else
+                Debug.WriteLine(lc);
+        }
+
+        Debug.WriteLine("############## Done with FizzBuzz, sucka ##############\n");
+    }
+}
